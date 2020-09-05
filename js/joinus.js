cd32e3ad8cf714c5c44718a50c10af581d7f3e68
lauren-mangold <63663070+lauren-mangold@users.noreply.github.com>
2020-09-05 16:58:13 -0500

    button hover function

--- js/joinus.js
+++ js/joinus.js
@@ -1,10 +1,15 @@
-console.log(
-$("button").hover(function(){
-    $(this).css("border", "black");
-});
-    $(this).css("background-color", "red");
+console.log("Hello world");
 
-})
+$(".buttons").hover(function(){
+    $(this).css("text-shadow", "2px 2px 4px #000000");
+    $(this).css("border", "2px black");
+    $(this).css("box-shadow", "2px 2px 4px #000000");
+},
+function(){
+    $(this).css("text-shadow", "none");
+    $(this).css("border", "none");
+    $(this).css("box-shadow", "none");
+});
 
 $( "#calendar" ).scroll(function() {
     $( "#log" ).append( "<div>Handler for .scroll() called.</div>" );
